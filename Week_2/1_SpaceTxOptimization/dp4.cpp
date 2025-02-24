f61ef89533744eb0b812b4bec4837a1e69c08e1b
Aryamann <aryamann.t@iitgn.ac.in>
2025-02-24 10:23:52 +0530

    Feb24_1023

--- Week_2/1_SpaceTxOptimization/dp4.cpp
+++ Week_2/1_SpaceTxOptimization/dp4.cpp
@@ -1,7 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-/*
-1 Book Shop [KnapSack Problem]
-Find out which subset of books has a total Price<=X and total no. of pages is maximised
-*/
+int main(){
+    int n, X;
+    cin>>n>>X;
+    vector<int> prices(n), pages(n);
+    for(auto &p: prices) cin>>p;
+    for(auto &p: pages) cin>>p;
+    vector<vector<int>> dp(n+1, vector<int>(X+1,0));
+    // for(int i=n-1;i>=0;i--){
+    //     for(int b=0;b<=X;b++){
+    //         int pick=0, skip=0;
+    //         if(b>=prices[i]) pick=pages[i]+dp[i+1][b-prices[i]];
+    //         skip=dp[i+1][b];
+    //         dp[i][b]=max(pick,skip);
+    //     }
+    // }
+    vector<int> curr(x+1), next(x+1,0);
+    for(int i=n-1;i>=0;i--){
+        for(int b=0;b<=X;b++){
+            int pick=0, skip=0;
+            if(b>=prices[i]) pick=pages[i]+next[b-prices[i]];
+            skip=next[b];
+            curr[b]=max(pick,skip);
+        }
+        next=curr;
+    }
+
+
+    cout<<dp[0][X];
+    /*
+    TC = num of states * TT per state
+    TC = O(N*X)*O(1)=>O(N*X)
+    n=1000 X=100000 TC=1e8 operations
+    dp[b][i] will give TLE
+    */
+    return 0;
+}
