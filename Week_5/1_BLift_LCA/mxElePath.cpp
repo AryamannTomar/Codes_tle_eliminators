0db1fa3be29dc77279db964311e5a7551bdd25b5
Aryamann Tomar <89705090+AryamannTomar@users.noreply.github.com>
2025-02-25 17:33:44 +0530

    Update mxElePath.cpp

--- Week_5/1_BLift_LCA/mxElePath.cpp
+++ Week_5/1_BLift_LCA/mxElePath.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
+    int n;cin>>n;
+    vector<int> a(n+1);
+    for(int i=0;i<=n-1;i++) cin>>a[i];
+    vector<vector<int>> adj(n+1);
+    for(int i=0;i<n-1;i++){
+        int u,v;cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
     const int LOG=18;
     vector<vector<int>> par(n+1,vector<int>(LOG));
     vector<int> depth(n+1);
@@ -74,18 +84,6 @@ int main(){
         int l=lca(u,v);
         return max(get_max(u,depth[u]-depth[l]), get_max(v,depth[v]-depth[l]),a[l]);
     };
-    /*
-    ============================================================================================
-	*/
-    int n;cin>>n;
-    vector<int> a(n+1);
-    for(int i=0;i<=n-1;i++) cin>>a[i];
-    vector<vector<int>> adj(n+1);
-    for(int i=0;i<n-1;i++){
-        int u,v;cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
 
     int q;cin>>q;
     while(q--){
