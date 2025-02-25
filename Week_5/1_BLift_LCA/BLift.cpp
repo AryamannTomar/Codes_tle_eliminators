f51a37382e7151e0e8f3b4ad2b1b77f278cff387
Aryamann <aryamann.t@iitgn.ac.in>
2025-02-25 13:06:45 +0530

    Feb_25_106

--- Week_5/1_BLift_LCA/BLift.cpp
+++ Week_5/1_BLift_LCA/BLift.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+    ancestor[u][k] -> 2^k-th ancestor of node u
+    anc[u][k] depends on anc[u][k-1] thats why k dimension comes outside the for loop
+*/
 void CalcAncestors(){
-    // ancestor[u][k] -> 2^k-th ancestor of node u
     for(int k=1;k<LOG;k++){
         for(int u=1;u<=n;u++){
             anc[u][k]=anc[anc[u][k-1]][k-1];
